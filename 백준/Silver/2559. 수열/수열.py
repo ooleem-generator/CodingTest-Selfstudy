463e099c123da198eae242d1198e8cd92372624b
Woojin Shin <swj7563@gmail.com>
2025-11-19 12:51:43 +0900

    [Silver III] Title: 수열, Time: 72 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,14 +1,12 @@
 n, k = map(int, input().split())
-temperatures = list(map(int, input().split()))
-dp = []
-dp.append(sum(temperatures[:k]))
-max_t = dp[0]
+temp = list(map(int, input().split()))
 
-if k == n:
-    print(max_t)
-else:
-    for i in range(1, n - k + 1):
-        dp.append(dp[i - 1] - temperatures[i - 1] + temperatures[i - 1 + k])
-        max_t = max(dp[i], max_t)
+a = sum(temp[:k])
+answer = a
 
-    print(max_t)
+if n > k:
+    for i in range(0, n - k):
+        a += temp[i + k] - temp[i]
+        answer = max(a, answer)
+
+print(answer)
